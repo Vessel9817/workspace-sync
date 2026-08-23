fac1653fe5c63ec1b5d5861d4964fe10c94360a9
Vessel9817 <151808241+Vessel9817@users.noreply.github.com>
2026-08-23 16:54:23 -0400

    fix: specify json type with json imports
    [Reference](https://nodejs.org/docs/latest-v24.x/api/esm.html#import-attributes)
    
    Signed-off-by: Vessel9817 <151808241+Vessel9817@users.noreply.github.com>

--- src/cli.ts
+++ src/cli.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 import { program } from 'commander';
-import * as file from '../package.json';
+import * as file from '../package.json' with { type: 'json' };
 import './commands';
 
 program
